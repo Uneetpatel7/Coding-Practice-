d7d06dcff92a2219ce4adbeecbf40c1c37cb235f
Uneet Patel <2015csb1038@iitrpr.ac.in>
2017-06-30 15:22:23 +0530

    README for the folder

---
+++ Room_booking_system/README.cpp
@@ -0,0 +1,4 @@
+Room.cpp contains the source code for the System.
+It takes the input from the text files room.txt, lab.txt and Lec.txt.
+Above .txt files contains the specifications such as room name, area , capacity, no. of computers in lab, no. of chairs in lecture hall, etc.
+After the code is run the final status  of the booked and unbooked Lab,Lec or room is outputed to OLab.txt,OLec.txt and ORoom.txt respectively
