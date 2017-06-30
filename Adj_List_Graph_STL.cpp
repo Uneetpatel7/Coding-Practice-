d4dfe9fb626ce594477747abde2f4adebc277c31
Uneet Patel <2015csb1038@iitrpr.ac.in>
2017-06-30 14:36:01 +0530

    Update Adj_List_Graph_STL.cpp

--- Adj_List_Graph_STL.cpp
+++ Adj_List_Graph_STL.cpp
@@ -8,33 +8,21 @@ using namespace std;
 int main()
 {
     int vertices, edges, v1, v2, weight;
-     
     printf("Enter the Number of Vertices -\n");
     scanf("%d", &vertices);
      
     printf("Enter the Number of Edges -\n");
     scanf("%d", &edges);
-     
-    // Adjacency List is a vector of list.
-    // Where each element is a pair<int, int>
-    // pair.first -> the edge's destination
-    // pair.second -> edge's weight
     vector< list< pair<int, int> > > adjacencyList(vertices + 1);
-     
     printf("Enter the Edges V1 -> V2, of weight W\n");
-     
     for (int i = 1; i <= edges; ++i) {
         scanf("%d%d%d", &v1, &v2, &weight);
-         
-        // Adding Edge to the Directed Graph
-        adjacencyList[v1].push_back(make_pair(v2, weight));
+        adjacencyList[i].push_back(make_pair(v2, weight));
     }
      
-    printf("\nThe Adjacency List-\n");
-    // Printing Adjacency List
+  
     for (int i = 1; i < adjacencyList.size(); ++i) {
-        printf("adjacencyList[%d] ", i);
-         
+        printf("adjacencyList[%d] ", i);    
         list< pair<int, int> >::iterator itr = adjacencyList[i].begin();
          
         while (itr != adjacencyList[i].end()) {
@@ -43,6 +31,5 @@ int main()
         }
         printf("\n");
     }
-     
     return 0;
 }
