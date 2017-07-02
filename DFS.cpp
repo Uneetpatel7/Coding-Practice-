7ca4f9c651719b925ef0bf50761fafaa798a8de8
Uneet Patel <2015csb1038@iitrpr.ac.in>
2017-07-03 00:35:40 +0530

    DFS in gaph implemented using Adjacency List DS.

---
+++ DFS.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Graph{
+private:
+	int no_of_vertices;
+	list<int> *adj_list;
+
+public:
+	Graph(int no_of_vertices);
+	void Add_Edge(int a, int b);
+	void DFS(int vertex);
+};
+
+Graph::Graph(int no_of_vertices){
+
+	this->no_of_vertices = no_of_vertices;
+	adj_list = new list<int> [no_of_vertices];
+
+}
+
+void Graph::Add_Edge(int v, int w){
+	adj_list[v].push_back(w);
+}
+
+void Graph::DFS(int vertex){
+
+	bool *visited = new bool[no_of_vertices];
+	memset(visited, 0, sizeof(visited));
+
+	stack<int> q;
+
+	q.push(vertex);
+	visited[vertex]=true;
+
+	list<int>::iterator itr;
+
+	while(!q.empty()){
+	vertex = q.top();
+	cout<< vertex << " ";
+	q.pop();	
+		for (itr = adj_list[vertex].begin(); itr != adj_list[vertex].end(); itr++){
+			if(!visited[*itr]){
+				q.push(*itr);
+				visited[*itr] = true;
+				break;
+			}
+		}	
+
+	} 
+
+}
+
+int main(){
+
+	Graph g(4);
+    g.Add_Edge(0, 1);
+    g.Add_Edge(0, 2);
+    g.Add_Edge(1, 2);
+    g.Add_Edge(2, 0);
+    g.Add_Edge(2, 3);
+    g.Add_Edge(3, 3);
+ 
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 2) n";
+    g.DFS(2);
+
+
+
+	return 0;
+}
+
